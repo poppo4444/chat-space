3daddfde5cef687eb14554407464849e438d838a
poppo4444 <poppo@ari.local>
2017-06-14 11:11:44 +0900

    change edit controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,13 @@ class UsersController < ApplicationController
   end
 
   def update
+    current_user.update(user_params)
   end
+
+  private
+  def user_params
+    params.permit(:name, :email)
+  end
+
+
 end
