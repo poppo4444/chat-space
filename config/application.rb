c743aaf299b0780b42ebeb66fc9e06027b4dbfbc
poppo4444 <poppo@ari.local>
2017-06-15 18:26:44 +0900

    Add device.ja.yml

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module ChatSpace
       g.test_framework false
       g.stylesheets false
     end
+
+
+  config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
