d099db7f00de7c5921f17987e91f47562d1385f4
poppo4444 <poppo@ari.local>
2017-06-15 15:15:40 +0900

    edit controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
@@ -14,7 +15,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root "messages#index"
-  resources :users, :only => [:update,:edit]
+  resources :users, only: [:update,:edit]
   devise_for :users
 end
 
