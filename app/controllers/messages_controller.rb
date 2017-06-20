26b00272ca841469fd4bf0b7cf807b74e554e50b
poppo4444 <poppo@ari.local>
2017-06-20 21:09:20 +0900

    make routing and action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,14 @@ class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
 end
