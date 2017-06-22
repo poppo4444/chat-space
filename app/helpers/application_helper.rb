598e9e63e9452661bcfbad97702522c16a69d76e
poppo4444 <poppo@ari.local>
2017-06-22 17:24:32 +0900

    make view helper group_messages_present?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def group_messages_present?(group)
+    if group.messages.last.present? 
+      group.messages.last.body
+    else
+      puts "最新のメッセージはありません"
+    end
+  end
+
 end
