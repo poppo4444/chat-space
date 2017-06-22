1368263fe0397b58e7c91a887cc3561a489a74a8
poppo4444 <poppo@ari.local>
2017-06-22 16:42:23 +0900

    change uploader and validate

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,7 @@
 class Message < ApplicationRecord
    mount_uploader :image, ImageUploader
 
-   validates :image_or_body, presence: true
+   validates :body, presence: true, unless: :image?
    belongs_to :group
    belongs_to :user
-
-   private
-   
-    def image_or_body
-      image.presence or body.presence
-    end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,15 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
+# リサイズしたり画像形式を変更するのに必要
   include CarrierWave::MiniMagick
-  require 'carrierwave/storage/fog'
+  # require 'carrierwave/storage/fog'
   
-  storage :fog
+  storage :file
 
-  process convert: 'jpg'
+  # 保存形式をJPGにする
+  # process convert: 'jpg'
 
+ # S3やローカルの保存先。
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -26,19 +29,26 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  # 画像を100x100にリサイズする。
+  # process :resize_to_fill => [100, 100]
+
+  # サムネイルを生成する設定
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [400, 200]
-  end
+  # version :thumb do
+  #   process resize_to_fit: [400, 200]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
+
+   # jpg,jpeg,gif,pngしか受け付けない
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
+    # 拡張子が同じでないとPNGをJPGとかにコンバートできないので、ファイル名を変更
   def filename
     "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename
   end
