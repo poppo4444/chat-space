756f9810dbce1adb7ae51744b316487139778851
poppo4444 <poppo@ari.local>
2017-06-23 20:34:42 +0900

    validate presence group_id & user_id

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
    mount_uploader :image, ImageUploader
 
    validates :body, presence: true, unless: :image?
+   validates :group_id, presence: true
+   validates :user_id,presence: true
    belongs_to :group
    belongs_to :user
 
