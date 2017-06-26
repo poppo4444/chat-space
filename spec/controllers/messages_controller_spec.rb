1aa16d2e7dfb498b795410889bb8a693a29bcc4c
poppo4444 <poppo@ari.local>
2017-06-26 13:34:00 +0900

    controller test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,34 +1,95 @@
-equire 'rails_helper'
+require 'rails_helper'
 
 describe MessagesController, type: :controller do
-  #ログイン時
-  describe 'GET #index ' do
-    it "アクション内で定義しているインスタンス変数があるか" do
+  let(:user) {create(:user)}
+  let(:message){build(:message)}
+  let(:group){create(:group)}
+  let(:message_params){{
+    message: attributes_for(:message),
+    group_id: group.id,
+    user_id: user.id
+  }}
+    let(:invalid){{
+    message: attributes_for(:message,{body: nil,image: nil}),
+    group_id: group.id,
+    user_id: user.id
+  }}
+
+    describe 'GET #index ' do
+      context "ログインしている時" do
+        before do 
+          login_user user
+          get :index, params: { group_id: group.id }
+        end
+
+        it "@groupが期待される値" do
+          expect(assigns(:group)).to eq group
+        end
+
+        it "@messageが期待される値" do
+          expect(assigns(:message)).to be_a_new(Message)
+        end
+
+        it "indexで定義している@messagesは作用するのか" do
+          messages = create_list(:message, 30, group_id: group.id)
+          expect(assigns(:messages)).to match(messages.sort{ |a,b| b.created_at <=> a.created_at})
+        end
+
+        it "indexのビューに遷移するか" do
+          expect(response).to render_template :index
+        end
+      end
+      
+      context "ログインしていない時" do
+        it "意図したビューにリダイレクトできているか" do
+          get :index, params: { group_id: group.id}
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
     end
 
-    it "該当するビューが描画されているか" do
-    end
-  #ログインしていない時
-    it "意図したビューにリダイレクトできているか" do
-    end
-    
-  end
+    describe 'POST #create' do
+      before do 
+      login_user user
+      end
+        context "ログインしていて保存に成功した時" do
+          before do
+            post :create, params: message_params
+          end
+          it "メッセージ保存成功するか" do
+            expect do
+              post :create, params: message_params
+              end.to change(Message, :count)
+          end
 
-  describe 'ログインしているかつ、保存に成功した時 ' do
-    
-    it "メッセージは保存されたか" do
-    end
-    
-    it "意図した画面に遷移しているか" do
-    end
-  end
-  
-  describe 'ログインしているが、保存に失敗した場合' do
-    it "メッセージの保存は行われなかったか" do
-    end
+          it "メッセージ保存成功時にgroup/indexに遷移するか" do
+              expect(response).to redirect_to group_messages_path(group.id)
+          end
 
-    it "意図したビューが描画されているか" do
+          it "メッセージ保存成功時にフラッシュメッセージが表示されるか" do
+              expect(flash[:notice]).to include("メッセージが作成されました")
+          end
+        end
+
+        context "ログインしているが保存に失敗した時" do
+          before do
+          post :create, params: invalid
+          end
+          
+          it "保存に失敗したか" do
+            expect do
+              post :create, params: invalid
+              end.not_to change(Message, :count)
+          end
+
+          it "メッセージ保存失敗時にgroup/indexに遷移するか" do
+            post :create, params: invalid
+            expect(response).to render_template :index
+          end
+
+          it "メッセージ保存失敗時にフラッシュメッセージが表示されるか" do
+            expect(flash[:alert]).to include("エラーが発生しました")
+          end
+        end
     end
-  end
-  
 end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -6,6 +6,8 @@ FactoryGirl.define do
     body              { Faker::Lorem.sentence}
     user_id           { Faker::Number.number(2)}
     group_id          { Faker::Number.number(2)}
+    created_at        { Faker::Time.between(5.days.ago, 3.days.ago, :all) }
+    updated_at        { Faker::Time.between(2.days.ago, 1.days.ago, :all) }
   end
 
 end
