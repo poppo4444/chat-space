db70f1d1af620bc98c26b748c686cf53bb5a1c8c
poppo4444 <poppo@ari.local>
2017-06-26 14:10:02 +0900

    indent

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -15,47 +15,46 @@ describe MessagesController, type: :controller do
     user_id: user.id
   }}
 
-    describe 'GET #index ' do
-      context "ログインしている時" do
-        before do 
-          login_user user
-          get :index, params: { group_id: group.id }
-        end
-
-        it "@groupが期待される値" do
-          expect(assigns(:group)).to eq group
-        end
-
-        it "@messageが期待される値" do
-          expect(assigns(:message)).to be_a_new(Message)
-        end
-
-        it "indexで定義している@messagesは作用するのか" do
-          messages = create_list(:message, 30, group_id: group.id)
-          expect(assigns(:messages)).to match(messages.sort{ |a,b| b.created_at <=> a.created_at})
-        end
-
-        it "indexのビューに遷移するか" do
-          expect(response).to render_template :index
-        end
+  describe 'GET #index ' do
+    context "ログインしている時" do
+      before do 
+        login_user user
+        get :index, params: { group_id: group.id }
       end
-      
-      context "ログインしていない時" do
-        it "意図したビューにリダイレクトできているか" do
-          get :index, params: { group_id: group.id}
-          expect(response).to redirect_to new_user_session_path
-        end
+
+      it "@groupが期待される値" do
+        expect(assigns(:group)).to eq group
+      end
+
+      it "@messageが期待される値" do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it "indexで定義している@messagesは作用するのか" do
+        messages = create_list(:message, 30, group_id: group.id)
+        expect(assigns(:messages)).to match(messages.sort{ |a,b| b.created_at <=> a.created_at})
+      end
+
+      it "indexのビューに遷移するか" do
+        expect(response).to render_template :index
       end
     end
-
-    describe 'POST #create' do
-      before do 
-      login_user user
+    context "ログインしていない時" do
+      it "意図したビューにリダイレクトできているか" do
+        get :index, params: { group_id: group.id}
+        expect(response).to redirect_to new_user_session_path
       end
-        context "ログインしていて保存に成功した時" do
-          before do
-            post :create, params: message_params
-          end
+    end
+  end
+
+  describe 'POST #create' do
+    before do 
+    login_user user
+    end
+      context "ログインしていて保存に成功した時" do
+        before do
+          post :create, params: message_params
+        end
           it "メッセージ保存成功するか" do
             expect do
               post :create, params: message_params
@@ -69,13 +68,11 @@ describe MessagesController, type: :controller do
           it "メッセージ保存成功時にフラッシュメッセージが表示されるか" do
               expect(flash[:notice]).to include("メッセージが作成されました")
           end
+      end
+      context "ログインしているが保存に失敗した時" do
+        before do
+        post :create, params: invalid
         end
-
-        context "ログインしているが保存に失敗した時" do
-          before do
-          post :create, params: invalid
-          end
-          
           it "保存に失敗したか" do
             expect do
               post :create, params: invalid
@@ -90,6 +87,6 @@ describe MessagesController, type: :controller do
           it "メッセージ保存失敗時にフラッシュメッセージが表示されるか" do
             expect(flash[:alert]).to include("エラーが発生しました")
           end
-        end
-    end
+      end
+  end
 end
