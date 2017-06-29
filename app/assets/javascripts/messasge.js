696be0261a840860b9af85fd5c673ddb120127cf
poppo4444 <poppo@ari.local>
2017-06-29 17:33:12 +0900

    メソッド名の修正

--- app/assets/javascripts/messasge.js
+++ app/assets/javascripts/messasge.js
@@ -1,34 +1,36 @@
 $(function(){
-  function buildHTML(message){
+  function haveimage(message){
 
     var html = `
       <p class="message--space--username"> ${message.user_name} </p>
-      <a class="message--space--timesstamp"> ${message.created_at} </a>
+      <span class="message--space--timesstamp"> ${message.created_at} </span>
       <p class="message--space--text"> ${message.body} </p>
       <img class="message--space--image" src="${message.image}"" </p>
       `
     return html;
   }
 
-  function build2HTML(message){
+  function noimgae(message){
     var html = `
       <p class="message--space--username"> ${message.user_name} </p>
-      <a class="message--space--timesstamp"> ${message.created_at} </a>
+      <span class="message--space--timesstamp"> ${message.created_at} </span>
       <p class="message--space--text"> ${message.body} </p>
       `
     return html;
   }
 
-  function prepend_and_form_val(html){
+  function prepend(html){
           $('.message').prepend(html)
+  }
+
+  function val(html){
           $('.tweet-box--form').val('')
   }
 
-  $('#new_message').on('submit', function(e){
+  $('.tweet--js').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var href = window.location.href;
-
     $.ajax({
       url: href,
       type: "POST",
@@ -41,12 +43,14 @@ $(function(){
 
     .done(function(data){
         if ( data.image == null ){
-          var html = buildHTML(data);
-          prepend_and_form_val(html)
+          var html = noimage(data);
+          prepend(html)
+          val(html)
           }
         else {
-          var html = build2HTML(data);
-          prepend_and_form_val(html)
+          var html = haveimage(data);
+          prepend(html)
+          val(html)
           }
       })
     .fail(function(){
