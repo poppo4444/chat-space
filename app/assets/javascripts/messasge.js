a51d984e83b085069ea5ef5f1868d87943a3b31b
poppo4444 <poppo@ari.local>
2017-06-29 18:22:43 +0900

    インデントの修正

--- app/assets/javascripts/messasge.js
+++ app/assets/javascripts/messasge.js
@@ -1,50 +1,46 @@
 $(function(){
   function haveimage(message){
-
     var html = `
       <p class="message--space--username"> ${message.user_name} </p>
       <span class="message--space--timesstamp"> ${message.created_at} </span>
       <p class="message--space--text"> ${message.body} </p>
       <img class="message--space--image" src="${message.image}"" </p>
-      `
+      `;
     return html;
   }
-
   function noimgae(message){
     var html = `
       <p class="message--space--username"> ${message.user_name} </p>
       <span class="message--space--timesstamp"> ${message.created_at} </span>
       <p class="message--space--text"> ${message.body} </p>
-      `
+      `;
     return html;
   }
-
   $('.tweet--js').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var href = window.location.href;
     $.ajax({
-    url: href,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
     .done(function(data){
-    if ( data.image === null ){
-    var html = noimage(data);
-    $('.message').prepend(html)
-    $('.tweet-box--form').val('')
-    }
-    else {
-    var html = haveimage(data);
-    $('.message').prepend(html)
-    $('.tweet-box--form').val('')
-    }
-      })
+      if ( data.image === null ){
+      var html = noimage(data);
+      $('.message').prepend(html);
+      $('.tweet-box--form').val('');
+      }else {
+      var html = haveimage(data);
+      $('.message').prepend(html);
+      $('.tweet-box--form').val('');
+      }
+    })
     .fail(function(){
-    alert('error');
+      alert('error');
     })
   })
 });
