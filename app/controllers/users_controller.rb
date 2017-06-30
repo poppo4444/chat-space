c7b8db0f020fd9d5873c39bbdc0e14ff63a3fa93
poppo4444 <poppo@ari.local>
2017-06-30 17:29:17 +0900

    スコープの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%")
+    @users = User.serch_users(params[:keyword])
     respond_to do |format|
       format.html
       format.json
