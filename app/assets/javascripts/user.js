856d968630475c2bd97150ad93643c3fe376b81a
poppo4444 <poppo@ari.local>
2017-06-30 17:50:20 +0900

    if文をメソッドへ移動

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,27 +2,27 @@ $(function(){
 
 var user_serch_list = $("#user-search-result");
 
-function appendUser(user){
-  var html = `<div class="chat-group-user clearfix">
-                <div class="chat-group-form__field--left">
-                  <div class="chat-group-form__label"></div>
-                </div>
-                <div class="chat-group-form__field--right">
-                  <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id}" data-user-name="${ user.name }">追加</a>
-                </div>
-              </div> `;
-    user_serch_list.append(html);
-}
-
-function appendNoUser(user){
-  var html = `<div class="chat-group-user clearfix">
+function incremental_serch(users){
+  if (users !== 0){
+    console.log(users)
+    var html = `<div class="chat-group-user clearfix">
+                  <div class="chat-group-form__field--left">
+                    <div class="chat-group-form__label"></div>
+                  </div>
+                  <div class="chat-group-form__field--right">
+                    <p class="chat-group-user__name">${ users.name }</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ users.id}" data-user-name="${ users.name }">追加</a>
+                  </div>
+                </div> `;
+  }else{
+    var html = `<div class="chat-group-user clearfix">
                 <div class="chat-group-form__field--left"></div>
                 <div class="chat-group-form__field--right">
                   "${user}"
                 </div>
               </div> `;
-    user_serch_list.append(html);
+  }
+      user_serch_list.append(html);
 }
 
   $(" .chat-group-form__search.clearfix").on('keyup', function(){
@@ -34,14 +34,10 @@ function appendNoUser(user){
       dataType: 'json'
     })
     .done(function(users){
-      $("#user-search-result").empty();
-      if(users.length !== 0){
-        users.forEach(function(user){
-        appendUser(user);
-        });
-      }else{
-        appendNoUser("一致するユーザーはいません");
-      }
+      user_serch_list.empty();
+      users.forEach(function(users){
+      incremental_serch(users);
+      });
     })
     .fail(function(){
       alert('error');
