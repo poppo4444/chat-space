6ddfc7812c6f8db3c2337666ef52c64a82dbee22
poppo4444 <poppo@ari.local>
2017-06-30 17:29:27 +0900

    インデント

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,23 +4,23 @@ var user_serch_list = $("#user-search-result");
 
 function appendUser(user){
   var html = `<div class="chat-group-user clearfix">
-              <div class="chat-group-form__field--left">
-              <div class="chat-group-form__label"></div>
-              </div>
-              <div class="chat-group-form__field--right">
-              <p class="chat-group-user__name">${ user.name }</p>
-              <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id}" data-user-name="${ user.name }">追加</a>
-              </div>
+                <div class="chat-group-form__field--left">
+                  <div class="chat-group-form__label"></div>
+                </div>
+                <div class="chat-group-form__field--right">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id}" data-user-name="${ user.name }">追加</a>
+                </div>
               </div> `;
     user_serch_list.append(html);
 }
 
 function appendNoUser(user){
   var html = `<div class="chat-group-user clearfix">
-              <div class="chat-group-form__field--left"></div>
-              <div class="chat-group-form__field--right">
-                "${user}"
-              </div>
+                <div class="chat-group-form__field--left"></div>
+                <div class="chat-group-form__field--right">
+                  "${user}"
+                </div>
               </div> `;
     user_serch_list.append(html);
 }
