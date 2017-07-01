d3493158dafe133759ab56aa56d9f7aefaf33ced
poppo4444 <poppo@ari.local>
2017-07-01 16:08:56 +0900

    インクリメンタルサーチの後の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,10 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
   before_action :authenticate_user!
 
+  def index
+    @groups = Group.all
+  end
+
   def new
     @group = Group.new
   end
